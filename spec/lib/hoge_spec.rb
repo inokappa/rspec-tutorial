f30b0bc40f4d1d7b11606792dc61e3204e9f127e
kappa <inokara@gmail.com>
2016-01-03 17:24:50 +0900

    modified

--- spec/lib/hoge_spec.rb
+++ spec/lib/hoge_spec.rb
@@ -5,7 +5,7 @@ describe Client do
   describe '#initialize' do
     context '引数 uri が設定されている場合' do
       before do
-        @uri = 'unix:///var/lib/lxd/unix.socket'
+        @uri = '/path/to/uri'
       end
       it '引数は文字列である' do
         expect(@uri).to be_a_kind_of(String)
@@ -21,7 +21,7 @@ describe Client do
   describe '#client' do
     context '引数 uri が設定されている場合' do
       before do
-        @uri = 'unix:///var/lib/lxd/unix.socket'
+        @uri = '/path/to/uri'
       end
       it 'object が生成される' do
         c = Client.new(@uri)
@@ -45,11 +45,17 @@ describe Client do
 
     context '引数 uri が正しく設定されている場合' do
       before do
-        @uri = 'unix:///var/lib/lxd/unix.socket'
+        uri = '/pat/to/uri'
+        @res = '{"type":"sync","status":"Success","status_code":200,"metadata":["/1.0/containers/test01"],"operation":""}'
+        @c = Client.new(uri)
+        allow(@c).to receive(:list_containers).and_return(@res)
+      end
+      it 'コンテナ一覧が String で取得出来る' do
+        expect(@c.list_containers).to be_a_kind_of(String)
       end
       it 'コンテナ一覧が取得出来る' do
-        c = Client.new(@uri)
-        expect(c.list_containers).to be_a_kind_of(String)
+        response = @c.list_containers
+        expect(response).to eq @res
       end
     end
 
