04d8c6e7fc309e27a7b91ed376cb8ad79dcdc6fd
kappa <inokara@gmail.com>
2016-01-03 22:35:48 +0900

    test

--- spec/lib/hoge_spec.rb
+++ spec/lib/hoge_spec.rb
@@ -34,21 +34,13 @@ describe Client do
     context '引数 uri が正しく設定されている場合' do
       before do
         uri = 'unix:///var/lib/lxd/unix.socket'
-        # @res = '{"type":"sync","status":"Success","status_code":200,"metadata":["/1.0/containers/test01"],"operation":""}'
+        @res = '{"type":"sync","status":"Success","status_code":200,"metadata":["/1.0/containers/test01"],"operation":""}'
         @c = Client.new(uri)
         allow(@c).to receive(:list_containers).and_return(@res)
       end
-      #it 'コンテナ一覧が String で取得出来る' do
-      #  expect(@c.list_containers).to be_a_kind_of(String)
-      #end
-
-      before do
-        uri = 'unix:///var/lib/lxd/unix.socket'
-        # @res = '{"type":"sync","status":"Success","status_code":200,"metadata":["/1.0/containers/test01"],"operation":""}'
-        @c = Client.new(uri)
-        allow(@c).to receive(:list_containers).and_return(@res)
+      it 'コンテナ一覧が String で取得出来る' do
+        expect(@c.list_containers).to be_a_kind_of(String)
       end
-
       it 'コンテナ一覧が取得出来る' do
         response = @c.list_containers
         expect(response).to eq @res
